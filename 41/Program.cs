62b11a18ccb38ecdbb069809c177577ca252432d
Denis <i9852829292@yandex.ru>
2022-12-05 09:37:25 +0100

    Домашнее задание

---
+++ 41/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 41: 
+// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+int[] array = new int[5];
+
+void FillArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"Введите элемент массива под индексом {i}: ");
+        arr[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+
+void PrintArray(int[]arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(i < arr.Length -1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+int GreaterZero(int[] arr)
+{
+    int res = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i] > 0) 
+        res ++;
+    }
+    return res;
+}
+
+FillArray(array); 
+PrintArray(array);
+int result = GreaterZero(array);
+Console.Write($" -> {result}");
---
+++ 43/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 43: 
+// Напишите программу, 
+// которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.WriteLine("Введите знаяения первой прямой: ");
+Console.Write("b1: ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("k1: ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите знаяения второй прямой: ");
+Console.Write("b2: ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("k2: ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double IntersectionTwoLines1 (double bx, double kx)
+{
+    double x = -0.5;
+    double y1 = kx * x + bx ; 
+    return (y1);
+}
+
+double IntersectionTwoLines2 (double by, double ky)
+{
+    double x = -0.5; 
+    double y2 = ky * x + by;
+    return (y2);
+}
+
+double intTwoLin1 = IntersectionTwoLines1(b1, k1);
+double intTwoLin2 = IntersectionTwoLines2(b2, k2);
+Console.WriteLine($"b1 = {b1}, k1 = {k1}, b2 = {b2}, k2 = {k2} -> ({intTwoLin1}; {intTwoLin2})");
